bed6907455e16fa05ddba144572674d79a3ec916
jyothi naidu <jyothinaidu@users.noreply.github.com>
2020-07-08 22:37:26 +0530

    Adding git ignore

--- Farm_Basket/Farm_Basket/settings.py
+++ Farm_Basket/Farm_Basket/settings.py
@@ -82,25 +82,25 @@ WSGI_APPLICATION = 'Farm_Basket.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'farm_basket',
-        'HOST': '127.0.0.1',
-        'PORT': '3307',
-        'USER': 'root',
-        'PASSWORD': 'vishal',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'farm_basket',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3307',
+#         'USER': 'root',
+#         'PASSWORD': 'vishal',
+#     }
+# }
 
-'''
+# '''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+# '''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
